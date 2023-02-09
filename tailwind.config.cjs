9b54e4bf81d182748b84eba4af9fd8ce7677247b
mami-em <maminiainaravaloarison@gmail.com>
2023-02-09 12:38:05 -0500

    font set, svg needed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'opensans': ['Open Sans', 'sans-serif'],
+        'codepro': ['Source Code Pro', 'monospace'],
+        'ubuntu': ['Ubuntu', 'sans-serif'],
+        'gloria': ['Gloria Hallelujah', 'cursive']
+      }
+    },
   },
   plugins: [],
 }
